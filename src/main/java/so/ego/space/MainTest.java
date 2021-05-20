e4a3f8afa7c0063a61a3d588e159330e0e3b9491
JISU-YANG <jisuyang@egoist.im>
2021-05-20 13:12:44 +0900

    [ADD] JPA TEST
    JPA가 잘 작동하는지 테스트했습니다.

---
+++ src/main/java/so/ego/space/MainTest.java
@@ -0,0 +1,12 @@
+package so.ego.space;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("테스형");
+        SpringApplication.run(MainTest.class, args);
+    }
+}
